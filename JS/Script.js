d9bdcb3679cdd39e8abeb7a4568a722100f06e7a
Josias Sanchez <softdevlearn@gmail.com>
2022-02-24 01:16:53 -0400

    Subiendo ejemplo de como hacer efecto parallax

---
+++ JS/Script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var barra = $(window).scrollTop();
+        var posicion = (barra * 0.10);
+
+        $('body').css({
+            'background-position': '0 -' + posicion + 'px'
+        });
+    });
+});
